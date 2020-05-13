c5ec8d4e806029327c0ff38cea349a79f96e0cac
Seth Villa <sethvilla13@gmail.com>
2020-05-13 01:01:17 -0400

    body

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="container">
    <HeroBanner/>
+    <section class="main_body">
+
+    <section class="aboutUsHome">
+
+      <section class="AbLeftHome">
+
+        <section class="photoAboutLeft">
+
+
+          <img class="imageAbout" src="https://images.pexels.com/photos/54276/pexels-photo-54276.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+
+
+        </section>
+
+        <section class="photoAboutRight">
+
+          <img class="imageAbout" src="https://images.pexels.com/photos/54276/pexels-photo-54276.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+
+
+          <img class="imageAbout" src="https://images.pexels.com/photos/54276/pexels-photo-54276.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+
+        </section>
+
+      </section>
+
+      <section class="AbRightHome">
+
+        <h2>Movement</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+
+      </section>
+
+    </section>
+
+  </section>
+
+
   </div> 
 </template>
 
@@ -18,4 +55,74 @@ import HeroBanner from './HeroBanner.vue';
   h3 {
     margin-bottom: 5%;
   }
+
+  .main_body {
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+}
+
+.aboutUsHome {
+  display: flex;
+flex-flow: row nowrap;
+height: 100vh;
+width: 100vw;
+}
+
+.AbLeftHome {
+  display: flex;
+  flex-basis: 66.66%;
+}
+
+.photoAboutLeft {
+
+  flex-basis: 50%;
+  display: flex;
+justify-content: center;
+align-items: center;
+
+}
+
+.photoAboutRight{
+
+  display: flex;
+  flex-basis: 50%;
+  flex-flow: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+}
+
+.imageAbout {
+  height: 250px
+}
+
+
+.AbRightHome {
+
+  display: flex;
+  flex-basis: 33.33%;
+  justify-content: center;
+  flex-flow: column;
+  align-items: center
+
+}
+
+.AbRightHome p {
+padding: 0 22%;
+}
+
+
+.backgroundBlack {
+  background-color: black;
+  flex-basis: 75%;
+}
+
+.square25 {
+  flex-basis: 25%;
+}
 </style>
